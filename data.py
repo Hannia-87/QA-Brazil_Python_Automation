9320921e78ff3dc71ae12ed3ef1173f7e9b9a897
Hannia-87 <hanniaqa17@gmail.com>
2025-07-29 18:28:53 -0300

    Projeto sprint 7

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
-# Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-90579634-c4dc-4524-bf71-d5f9a8ea3606.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = 	'1300 1st St'
+PHONE_NUMBER = 	'+1 123 123 12 12'
+CARD_NUMBER = 	'1234 5678 9100'
+CARD_CODE = 	'1111'
+MESSAGE_FOR_DRIVER = 	'Pare no bar de sucos'
+
