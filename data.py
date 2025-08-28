2a099d84968b7e6deaca102b4ee45c9d6bafe879
Hannia-87 <hanniaqa17@gmail.com>
2025-08-28 15:41:37 -0300

    Projeto sprint 8

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-URBAN_ROUTES_URL = 'https://cnt-90579634-c4dc-4524-bf71-d5f9a8ea3606.containerhub.tripleten-services.com?lng=pt'
+URBAN_ROUTES_URL = 'https://cnt-996363df-2a17-4fb0-92f4-24bde55cfa15.containerhub.tripleten-services.com?lng=pt'
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = 	'1300 1st St'
 PHONE_NUMBER = 	'+1 123 123 12 12'
